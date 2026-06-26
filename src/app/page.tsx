5df9ba4c50cf22d7868b82957ba4021050bc8860
Prathamesh Ugale <prathamesh.ugale@geloratech.com>
2026-06-26 15:43:09 +0530

    feat: temple page ui changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,3 @@
-// import HeroSection from "@/components/home/HeroSection";
-// import TemplesSection from "@/components/home/TemplesSection";
-// import ServicesSection from "@/components/home/ServicesSection";
-// import SubscriptionSection from "@/components/home/SubscriptionSection";
-// import HowItWorksSection from "@/components/home/HowItWorksSection";
-
-// export default function HomePage() {
-//   return (
-//     <main>
-//       {/* Section 1: Hero Banner with Live Darshan Countdown */}
-//       <HeroSection />
-
-//       {/* Section 2: Explore Sacred Temples Grid */}
-//       <TemplesSection />
-
-//       {/* Section 3: Our Devotional Services Grid */}
-//       <ServicesSection />
-
-//       {/* Section 4: Never Miss a Festival Update – Subscription Plans */}
-//       <SubscriptionSection />
-
-//       {/* Section 5: How Brajmarg Works – 4-Step Process */}
-//       <HowItWorksSection />
-//     </main>
-//   );
-// }
 import HeroSection from "@/components/home/HeroSection";
 import TemplesSection from "@/components/home/TemplesSection";
 import ServicesSection from "@/components/home/ServicesSection";
--- src/components/temples/TemplePage.tsx
+++ src/components/temples/TemplePage.tsx
@@ -1,34 +1,3 @@
-// import TempleHero from "./TempleHero";
-// import TempleSearch from "./TempleSearch";
-// import TempleGrid from "./TempleGrid";
-// import TempleCTA from "./TempleCTA";
-
-// export default function TemplePage() {
-//   return (
-//     <main className="min-h-screen bg-[#F7F0E5]">
-//       <section className="relative h-[340px] overflow-visible">
-//         <TempleHero />
-
-//         <div className="absolute bottom-[-200px] left-1/2 z-20 w-full max-w-[1180px] -translate-x-1/2 px-5 md:bottom-[-95px]">
-//           <TempleSearch />
-//         </div>
-//       </section>
-
-//       <section
-//         className="pt-[200px]"
-//         style={{ marginTop: "260px", marginLeft: "120px" }}
-//       >
-//         <TempleGrid />
-//       </section>
-//       <section
-//         className="pt-8"
-//         style={{ marginBottom: "50px", marginLeft: "120px" }}
-//       >
-//         <TempleCTA />
-//       </section>
-//     </main>
-//   );
-// }
 "use client";
 
 import { useState } from "react";
